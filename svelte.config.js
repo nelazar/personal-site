bfcef4fc2044788b9fb5a675f78c3cbab725fbe2
nelazar <noahbelazar@gmail.com>
2026-01-02 15:27:32 -0800

    Removed paths.base config

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,9 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	}
 };
 
